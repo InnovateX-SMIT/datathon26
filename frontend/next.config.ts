a6a6988a4dc8552029ccde67182ed1617c36fe71
abhinavgitin <prabhudarsanpuri987@gmail.com>
2026-07-21 16:24:39 +0530

    chore: rm unused files and folders, cleanup repo.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
@@ -9,7 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/app" : undefined,
 };
 
 export default nextConfig;
